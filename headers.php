19157e6988c5b3c9a25d2b506f9dfa9a401b0e2a
Kevin <kevin.pichette@gmail.com>
2016-05-26 22:04:17 -0700

    testing

---
+++ headers.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Plugin Name: Headers
+Description: Shortcode for headers
+Version: 2016.5.26.1
+Author: Kevin Pichette
+Author URI: http://www.kevinpichette.com
+License: GPL
+Copyright: Kevin Pichettte
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+add_shortcode('header','header_code');
+
+function header_code($atts, $content = null)
+{
+	$a = shortcode_atts(array(
+			'size' => '20px',
+			'weight' => 'bold',
+			'align' => 'center',
+		),$atts);
+
+	$size = $a['size'];
+	$weight = $a['weight'];
+	$align = $a['align'];
+
+	return '<div style="text-align: ' . $align . ';">
+				<div style="float: left; width: 30%; margin-top: 10px; overflow:hidden;">
+
+					<hr />
+
+				</div>
+				<div style="float: left; width: 30%;">
+					<h1 style="font-size: ' . $size . '; margin: auto;">' . $content . '</h1>
+				</div>
+				<div style="float: left; width: 30%; margin-top: 10px; overflow: hidden;">
+
+					<hr />
+
+				</div>
+				<div style="clear:left;"></div>
+			</div>';
+}
